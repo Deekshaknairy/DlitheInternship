22f01dab0ea916d2bd70c6a0f4596aab922cf9ca
deeksha k nairy <deekshaknairy@gmail.com>
2025-02-19 16:50:49 +0530

    python codes

---
+++ day1.py
@@ -0,0 +1,64 @@
+age=20
+if(age<=18):
+    print("he is younger")
+else:
+    print("he is mature")
+
+a='5'
+b='deeksha'
+print(a)
+print(b)
+res=a+b
+print(res)
+
+a=float(50)
+print(a)
+b=str(8.9)
+print(b)
+c=str(9)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+age="20"
+a='25'
+print(age)
+print(a)
+
+A="apple"
+a="cricket"
+print(A)
+print(a)
+
+a=b=c="RCB"
+print(a)
+print(b)
+print(c)
+
+a=["apple","kiwi","grapes"]
+x,y,z=a
+print(x)
+print(y)
+print(z)
+
+a="python"
+b="is"
+c="awesome"
+print(a,b,c)
+
+a=int(input("enter an integer:"))
+b=int(input("enter an integer:"))
+print(a+b)
+
+x=2
+y=5
+x,y=y,x
+print(x)
+print(y)
+
+x="deeksha"
+def myfunction():
+    print(x)
+myfunction()
---
+++ day2.py
@@ -0,0 +1,74 @@
+x="Deeksha"
+print(x)
+x="my name is 'deeksha'"
+print(x)
+x="my "name" is 'deeksha'"
+print(x)
+y='hello "dk"'
+print(y)
+a="today"
+print(a[1])
+a= "hello","world","welcome"
+print(a[1])
+a="beautiful"
+b="world"
+c="other"
+print(len(a),len(b),len(c))
+a="""world is nice
+how are you
+have a good day
+"""
+print(a)
+a=" hello world! welcome to the world"
+print("to" in a)
+print("long" in a)
+a=" hello world! welcome to the world"
+if "welcome" in a:
+    print(a)
+if "to" in a:
+    print(a)
+if "is" not in a:
+    print("does'nt exist")
+if "python" not in a:
+    print("does'nt exist")
+a="deeksha"
+print(a[1:5])
+b="my life is lovely"
+print(b[1:12])
+print(b[:5])
+b="my life my rules"
+print(b[9:])
+x="angles"
+print(x.upper())
+y="yeh tune kya kiya"
+print(y.upper())
+a="BAD BOY"
+print(a.lower())
+a="i live in banglore"
+print(a.replace("l","j"))
+print(a.replace("g","p"))
+a="hel,lo ! world"
+b=a.split(",")
+print(b)
+a="hello"
+b="chings"
+c="chinese"
+print(a+b+c)
+age=21
+b=f"my name is deeksha, my age is {age}"
+print(b)
+x="rcb"
+y="virat kholi"
+b=f"i support team {x}. i love {y}"
+print(b)
+print(10>5)
+print(9<5)
+print(50==45)
+print(12!=15)
+a=int(input("father's age:"))
+b=int(input("son's age:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
